9a39706b76f2bc97e6139a2063612b8cbd9237c6
T_dak <thilandakshina@gmail.com>
2025-02-15 13:18:31 +0530

    update

--- cafe-employee-manager/src/api/config.js
+++ cafe-employee-manager/src/api/config.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://localhost:5002/api';
-const BASE_URL = 'https://cafeapi-axb4fggjb5cshdag.australiasoutheast-01.azurewebsites.net/api';
+const BASE_URL = 'https://localhost:5002/api';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
