b709297abd0759f5d334365367d81bcd90b5b7db
T_dak <thilandakshina@gmail.com>
2025-02-15 15:15:46 +0530

    updated

--- CafeManagement/Cafe.Domain/Models/CafeWithEmployees.cs
+++ CafeManagement/Cafe.Domain/Models/CafeWithEmployees.cs
@@ -4,8 +4,8 @@ namespace Cafe.Domain.Models
 {
     public class CafeWithEmployees
     {
-        public CafeEntity Cafe { get; }
-        public ICollection<CafeEmployeeEntity> Employees { get; }
+        public CafeEntity Cafe { get; set; }
+        public ICollection<CafeEmployeeEntity> Employees { get; set; }
 
         public CafeWithEmployees(CafeEntity cafe, ICollection<CafeEmployeeEntity> employees)
         {
