1e6a5c4299ea20d58d9a62ebb0c98e8951390a47
T_dak <thilandakshina@gmail.com>
2025-02-15 13:36:42 +0530

    cs

--- CafeManagement/Cafe.API/Program.cs
+++ CafeManagement/Cafe.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Data Layer
+// Data Layer 
 
 builder.Services.AddDbContext<CafeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
